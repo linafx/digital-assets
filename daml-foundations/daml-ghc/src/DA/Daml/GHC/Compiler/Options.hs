snowsky/daml

fixing tests / move to depreceated

daml-foundations/daml-ghc/src/DA/Daml/GHC/Compiler/Options.hs
@@ -88,16 +88,19 @@ moduleImportPaths pm =
 -- | Check that import paths and package db directories exist
 -- and add the default package db if it exists
 mkOptions :: Options -> IO Options
-mkOptions opts@Options{..} = do
+mkOptions opts@Options {..} = do
     baseDir <- getBaseDir
     mapM_ checkDirExists $ optImportPath <> optPackageDbs
     let defaultPkgDb = baseDir </> "package-database"
+    let depreceatedPkgDb = baseDir </> "package-database" </> "depreceated"
     hasDefaultPkgDb <- Dir.doesDirectoryExist defaultPkgDb
-    pure opts
-            { optPackageDbs =
-                  map (</> versionSuffix) $
-                  [defaultPkgDb | hasDefaultPkgDb] ++ optPackageDbs
-            }
+    hasDepreceatedPkgDb <- Dir.doesDirectoryExist depreceatedPkgDb
+    let pkgDb
+            | hasDepreceatedPkgDb = [depreceatedPkgDb]
+            | hasDefaultPkgDb = [defaultPkgDb]
+            | otherwise = []
+    when hasDepreceatedPkgDb $ putStrLn "DEPRECEATED: Please use fat dars instead of dalfs in tests"
+    pure opts {optPackageDbs = map (</> versionSuffix) $ pkgDb ++ optPackageDbs}
   where checkDirExists f =
           Dir.doesDirectoryExist f >>= \ok ->
           unless ok $ error $

daml-lf/engine/src/test/scala/com/digitalasset/daml/lf/engine/EngineTest.scala
@@ -46,7 +46,7 @@ class EngineTest extends WordSpec with Matchers {
   private val (basicTestsPkgId, basicTestsPkg) =
     loadPackage("daml-lf/tests/BasicTests.dalf")
   private val (ghcPrimPkgId, ghcPrimPkg) =
-    loadPackage("daml-foundations/daml-ghc/package-database/daml-prim-1.1.dalf")
+    loadPackage("daml-foundations/daml-ghc/package-database/depreceated/daml-prim-1.1.dalf")
 
   private[this] def makeAbsoluteContractId(coid: ContractId): AbsoluteContractId =
     coid match {

daml-lf/engine/src/test/scala/com/digitalasset/daml/lf/engine/LargeTransactionTest.scala
@@ -19,7 +19,8 @@ import org.scalatest.{Assertion, Matchers, WordSpec}
 class LargeTransactionTest extends WordSpec with Matchers {
 
   private[this] val largeTransactionId = "daml-lf/tests/LargeTransaction.dalf"
-  private[this] val ghcPrimId = "daml-foundations/daml-ghc/package-database/daml-prim-1.1.dalf"
+  private[this] val ghcPrimId =
+    "daml-foundations/daml-ghc/package-database/depreceated/daml-prim-1.1.dalf"
 
   private[this] val largeTx: (PackageId, Ast.Package) = loadArchiveAsResource(largeTransactionId)
   private[this] val ghcPrim: (PackageId, Ast.Package) =

daml-lf/testing-tools/src/test/scala/com/digitalasset/daml/lf/engine/testing/SemanticTests.scala
@@ -24,7 +24,7 @@ class SemanticTests extends WordSpec with Matchers with ScalaFutures with Integr
   )
 
   val libraryDalfs = List(
-    "daml-foundations/daml-ghc/package-database/daml-prim-1.1.dalf"
+    "daml-foundations/daml-ghc/package-database/depreceated/daml-prim-1.1.dalf"
   )
 
   val testPkgs = dalfFiles.map(file => {
@@ -37,7 +37,9 @@ class SemanticTests extends WordSpec with Matchers with ScalaFutures with Integr
   val libraryPkgs = libraryDalfs.map(file => {
     val stream = loader.getResourceAsStream(file)
     (
-      file.stripPrefix("daml-foundations/daml-ghc/package-database").stripSuffix(".dalf"),
+      file
+        .stripPrefix("daml-foundations/daml-ghc/package-database/depreceated")
+        .stripSuffix(".dalf"),
       Decode.decodeArchiveFromInputStream(stream))
   })
 

daml-lf/tests/scenario/actualize.sh
@@ -16,7 +16,7 @@ TESTMAIN=$1
 TESTDIR="$(dirname $TESTMAIN)"
 TESTDALF="$TESTDIR/Main.dalf"
 BAZEL_BIN="$(bazel info bazel-bin)"
-GHC_PRIM_DALF=${BAZEL_BIN}/daml-foundations/daml-ghc/package-database/daml-prim-1.3.dalf
+GHC_PRIM_DALF=${BAZEL_BIN}/daml-foundations/daml-ghc/package-database/depreceated/daml-prim-1.3.dalf
 REGEX_HIDE_HASHES="s,@[a-z0-9]{8},@XXXXXXXX,g"
 
 bazel build //daml-foundations/daml-tools/da-hs-damlc-app:da-hs-damlc-app

language-support/scala/codegen-sample-app/src/test/scala/com/digitalasset/codegen/ScalaCodeGenIT.scala
@@ -84,7 +84,7 @@ class ScalaCodeGenIT
 
   private val archives = List(
     requiredResource("language-support/scala/codegen-sample-app/MyMain.dalf"),
-    requiredResource("daml-foundations/daml-ghc/package-database/daml-prim-1.1.dalf")
+    requiredResource("daml-foundations/daml-ghc/package-database/depreceated/daml-prim-1.1.dalf")
   )
 
   private val asys = ActorSystem()

ledger/ledger-api-integration-tests/src/test/lib/scala/com/digitalasset/platform/PlatformApplications.scala
@@ -68,7 +68,8 @@ object PlatformApplications {
     val defaultLedgerId = "ledger server"
 
     val defaultDarFile = new File("ledger/sandbox/Test.dalf")
-    val ghcPrimFileName = new File("daml-foundations/daml-ghc/package-database/daml-prim-1.3.dalf")
+    val ghcPrimFileName = new File(
+      "daml-foundations/daml-ghc/package-database/depreceated/daml-prim-1.3.dalf")
 
     val defaultParties = NonEmptyList("party", "Alice", "Bob")
     val defaultTimeProviderType = TimeProviderType.Static

ledger/sandbox/src/test/lib/scala/com/digitalasset/platform/sandbox/services/SandboxFixture.scala
@@ -31,7 +31,7 @@ trait SandboxFixture extends SuiteResource[Channel] {
   protected def darFile = new File("ledger/sandbox/Test.dalf")
 
   protected def ghcPrimFile =
-    new File("daml-foundations/daml-ghc/package-database/daml-prim-1.3.dalf")
+    new File("daml-foundations/daml-ghc/package-database/depreceated/daml-prim-1.3.dalf")
 
   protected def channel: Channel = suiteResource.value
 

linafx/digital-assets

daml-foundations/daml-ghc/src/DA/Daml/GHC/Compiler/Options.hs
@@ -88,16 +88,19 @@ moduleImportPaths pm =
 -- | Check that import paths and package db directories exist
 -- and add the default package db if it exists
 mkOptions :: Options -> IO Options
-mkOptions opts@Options{..} = do
+mkOptions opts@Options {..} = do
     baseDir <- getBaseDir
     mapM_ checkDirExists $ optImportPath <> optPackageDbs
     let defaultPkgDb = baseDir </> "package-database"
+    let deprecatedPkgDb = baseDir </> "package-database" </> "deprecated"
     hasDefaultPkgDb <- Dir.doesDirectoryExist defaultPkgDb
-    pure opts
-            { optPackageDbs =
-                  map (</> versionSuffix) $
-                  [defaultPkgDb | hasDefaultPkgDb] ++ optPackageDbs
-            }
+    hasDepreceatedPkgDb <- Dir.doesDirectoryExist deprecatedPkgDb
+    let pkgDb
+            | hasDepreceatedPkgDb = [deprecatedPkgDb]
+            | hasDefaultPkgDb = [defaultPkgDb]
+            | otherwise = []
+    when hasDepreceatedPkgDb $ putStrLn "DEPRECEATED: Please use fat dars instead of dalfs in tests"
+    pure opts {optPackageDbs = map (</> versionSuffix) $ pkgDb ++ optPackageDbs}
   where checkDirExists f =
           Dir.doesDirectoryExist f >>= \ok ->
           unless ok $ error $

daml-lf/engine/src/test/scala/com/digitalasset/daml/lf/engine/EngineTest.scala
@@ -46,7 +46,7 @@ class EngineTest extends WordSpec with Matchers {
   private val (basicTestsPkgId, basicTestsPkg) =
     loadPackage("daml-lf/tests/BasicTests.dalf")
   private val (ghcPrimPkgId, ghcPrimPkg) =
-    loadPackage("daml-foundations/daml-ghc/package-database/daml-prim-1.1.dalf")
+    loadPackage("daml-foundations/daml-ghc/package-database/deprecated/daml-prim-1.1.dalf")
 
   private[this] def makeAbsoluteContractId(coid: ContractId): AbsoluteContractId =
     coid match {

daml-lf/engine/src/test/scala/com/digitalasset/daml/lf/engine/LargeTransactionTest.scala
@@ -19,7 +19,8 @@ import org.scalatest.{Assertion, Matchers, WordSpec}
 class LargeTransactionTest extends WordSpec with Matchers {
 
   private[this] val largeTransactionId = "daml-lf/tests/LargeTransaction.dalf"
-  private[this] val ghcPrimId = "daml-foundations/daml-ghc/package-database/daml-prim-1.1.dalf"
+  private[this] val ghcPrimId =
+    "daml-foundations/daml-ghc/package-database/deprecated/daml-prim-1.1.dalf"
 
   private[this] val largeTx: (PackageId, Ast.Package) = loadArchiveAsResource(largeTransactionId)
   private[this] val ghcPrim: (PackageId, Ast.Package) =

daml-lf/testing-tools/src/test/scala/com/digitalasset/daml/lf/engine/testing/SemanticTests.scala
@@ -24,7 +24,7 @@ class SemanticTests extends WordSpec with Matchers with ScalaFutures with Integr
   )
 
   val libraryDalfs = List(
-    "daml-foundations/daml-ghc/package-database/daml-prim-1.1.dalf"
+    "daml-foundations/daml-ghc/package-database/deprecated/daml-prim-1.1.dalf"
   )
 
   val testPkgs = dalfFiles.map(file => {
@@ -37,7 +37,9 @@ class SemanticTests extends WordSpec with Matchers with ScalaFutures with Integr
   val libraryPkgs = libraryDalfs.map(file => {
     val stream = loader.getResourceAsStream(file)
     (
-      file.stripPrefix("daml-foundations/daml-ghc/package-database").stripSuffix(".dalf"),
+      file
+        .stripPrefix("daml-foundations/daml-ghc/package-database/deprecated")
+        .stripSuffix(".dalf"),
       Decode.decodeArchiveFromInputStream(stream))
   })
 

daml-lf/tests/scenario/actualize.sh
@@ -16,7 +16,7 @@ TESTMAIN=$1
 TESTDIR="$(dirname $TESTMAIN)"
 TESTDALF="$TESTDIR/Main.dalf"
 BAZEL_BIN="$(bazel info bazel-bin)"
-GHC_PRIM_DALF=${BAZEL_BIN}/daml-foundations/daml-ghc/package-database/daml-prim-1.3.dalf
+GHC_PRIM_DALF=${BAZEL_BIN}/daml-foundations/daml-ghc/package-database/deprecated/daml-prim-1.3.dalf
 REGEX_HIDE_HASHES="s,@[a-z0-9]{8},@XXXXXXXX,g"
 
 bazel build //daml-foundations/daml-tools/da-hs-damlc-app:da-hs-damlc-app

language-support/scala/codegen-sample-app/src/test/scala/com/digitalasset/codegen/ScalaCodeGenIT.scala
@@ -84,7 +84,7 @@ class ScalaCodeGenIT
 
   private val archives = List(
     requiredResource("language-support/scala/codegen-sample-app/MyMain.dalf"),
-    requiredResource("daml-foundations/daml-ghc/package-database/daml-prim-1.1.dalf")
+    requiredResource("daml-foundations/daml-ghc/package-database/deprecated/daml-prim-1.1.dalf")
   )
 
   private val asys = ActorSystem()

ledger/ledger-api-integration-tests/src/test/lib/scala/com/digitalasset/platform/PlatformApplications.scala
@@ -68,7 +68,8 @@ object PlatformApplications {
     val defaultLedgerId = "ledger server"
 
     val defaultDarFile = new File("ledger/sandbox/Test.dalf")
-    val ghcPrimFileName = new File("daml-foundations/daml-ghc/package-database/daml-prim-1.3.dalf")
+    val ghcPrimFileName = new File(
+      "daml-foundations/daml-ghc/package-database/deprecated/daml-prim-1.3.dalf")
 
     val defaultParties = NonEmptyList("party", "Alice", "Bob")
     val defaultTimeProviderType = TimeProviderType.Static

ledger/sandbox/src/test/lib/scala/com/digitalasset/platform/sandbox/services/SandboxFixture.scala
@@ -31,7 +31,7 @@ trait SandboxFixture extends SuiteResource[Channel] {
   protected def darFile = new File("ledger/sandbox/Test.dalf")
 
   protected def ghcPrimFile =
-    new File("daml-foundations/daml-ghc/package-database/daml-prim-1.3.dalf")
+    new File("daml-foundations/daml-ghc/package-database/deprecated/daml-prim-1.3.dalf")
 
   protected def channel: Channel = suiteResource.value
 