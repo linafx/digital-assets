c64f7dea91f0421579f3f96b99ddbfc22a2a2f44
Gabor Aranyossy <gabor.aranyossy@digitalasset.com>
2019-05-14 10:02:20 +0200

    a bit less vars

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/SandboxApplication.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/SandboxApplication.scala
@@ -37,28 +37,32 @@ object SandboxApplication {
 
     case class State(
         ledgerId: String,
-        port: Int,
         apiServer: LedgerApiServer,
         stopHeartbeats: () => Unit
-    )
+    ) {
+      def port: Int = apiServer.port
+    }
 
     case class InfraState(
         actorSystem: ActorSystem,
         materializer: ActorMaterializer,
-        metricsManager: MetricsManager) {
+        metricsManager: MetricsManager)
+        extends AutoCloseable {
       def executionContext: ExecutionContext = materializer.executionContext
+      def close: Unit = {
+        materializer.shutdown()
+        Await.result(actorSystem.terminate(), asyncTolerance)
+        metricsManager.close()
+      }
     }
 
+    @volatile private var state: State = _
     @volatile private var infraState: InfraState = _
-
-    //TODO: get rid of these vars! Stateful resources should be created as vals when the owner object is created.
-    @volatile private var server: LedgerApiServer = _
-    @volatile private var ledgerId: String = _
-    @volatile private var stopHeartbeats: () => Unit = () => ()
-    @volatile var port: Int = config.port
 
     //TODO: why is this exposed???
     def getMaterializer: ActorMaterializer = infraState.materializer
+
+    def port: Int = state.port
 
     // We memoize the engine between resets so we avoid the expensive
     // repeated validation of the sames packages after each reset
@@ -66,21 +70,21 @@ object SandboxApplication {
 
     /** the reset service is special, since it triggers a server shutdown */
     private val resetService: SandboxResetService = new SandboxResetService(
-      () => ledgerId,
+      () => state.ledgerId,
       () => infraState.executionContext,
       () => resetAndRestartServer()
     )
 
     // returns with a Future firing when all services have been closed!
     private def resetAndRestartServer(): Future[Unit] = {
-      stopHeartbeats()
+      state.stopHeartbeats()
       //need to run this async otherwise the callback kills the server under the in-flight reset service request!
       Future {
-        server.close() // fully tear down the old server.
+        state.apiServer.close() // fully tear down the old server.
         buildAndStartServer(SqlStartMode.AlwaysReset)
       }(infraState.executionContext)
 
-      server.servicesClosed()
+      state.apiServer.servicesClosed()
     }
 
     @SuppressWarnings(Array("org.wartremover.warts.ExplicitImplicitTypes"))
@@ -90,7 +94,7 @@ object SandboxApplication {
       implicit val ec: ExecutionContext = infraState.executionContext
       implicit val mm: MetricsManager = infraState.metricsManager
 
-      ledgerId = config.ledgerIdMode.ledgerId()
+      val ledgerId = config.ledgerIdMode.ledgerId()
 
       val context = SandboxContext.fromConfig(config)
 
@@ -131,8 +135,8 @@ object SandboxApplication {
 
       val ledgerBackend = new SandboxLedgerBackend(ledger)
 
-      stopHeartbeats = scheduleHeartbeats(timeProvider, ledger.publishHeartbeat)
-      server = LedgerApiServer(
+      val stopHeartbeats = scheduleHeartbeats(timeProvider, ledger.publishHeartbeat)
+      val apiServer = LedgerApiServer(
         (am: ActorMaterializer, esf: ExecutionSequencerFactory) =>
           ApiServices
             .create(
@@ -147,20 +151,24 @@ object SandboxApplication {
                     ledger.publishHeartbeat
                   )))(am, esf)
             .withServices(List(resetService)),
-        port,
+        Option(state).fold(config.port)(_.port),
         config.address,
         config.tlsConfig.flatMap(_.server)
       )
 
       // NOTE(JM): Re-use the same port after reset.
-      port = server.port
+      state = State(
+        ledgerId,
+        apiServer,
+        stopHeartbeats
+      )
 
       Banner.show(Console.out)
       logger.info(
         "Initialized sandbox version {} with ledger-id = {}, port = {}, dar file = {}, time mode = {}, ledger = {}, daml-engine = {}",
         BuildInfo.Version,
         ledgerId,
-        port.toString,
+        state.port.toString,
         config.damlPackageContainer: AnyRef,
         config.timeProviderType,
         ledgerType
@@ -176,12 +184,8 @@ object SandboxApplication {
     }
 
     override def close(): Unit = {
-      stopHeartbeats()
-      Option(infraState).foreach { is =>
-        is.materializer.shutdown()
-        Await.result(is.actorSystem.terminate(), asyncTolerance)
-        is.metricsManager.close()
-      }
+      Option(state).foreach(_.stopHeartbeats)
+      Option(infraState).foreach(_.close())
       //TODO: stop ledger backend here instead!
     }
   }
