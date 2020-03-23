6dcfb7edb571e7601613de39fc39fc7188cf795d
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2020-03-23 15:54:45 +0100

    grpc debug
    changelog_begin
    changelog_end

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -8,19 +8,19 @@ let
 
   # package overrides
   overrides = _: pkgs: rec {
-    grpc = pkgs.grpc.overrideAttrs (oldAttrs: {
-      version = "1.23.1";
-      src = pkgs.fetchFromGitHub {
-        owner = "grpc";
-        repo = "grpc";
-        rev = "v1.23.1";
-        sha256 = "1jcyd9jy7kz5zfch25s4inwlivb1y1w52fzfjy5ra5vcnp3hmqyr";
-        fetchSubmodules = true;
-      };
-      # Upstream nixpkgs applies patches that are incompatbile with our version
-      # of grpc. So, we disable them.
-      patches = [];
-    });
+    # grpc = pkgs.grpc.overrideAttrs (oldAttrs: {
+    #   version = "1.23.1";
+    #   src = pkgs.fetchFromGitHub {
+    #     owner = "grpc";
+    #     repo = "grpc";
+    #     rev = "v1.23.1";
+    #     sha256 = "1jcyd9jy7kz5zfch25s4inwlivb1y1w52fzfjy5ra5vcnp3hmqyr";
+    #     fetchSubmodules = true;
+    #   };
+    #   # Upstream nixpkgs applies patches that are incompatbile with our version
+    #   # of grpc. So, we disable them.
+    #   patches = [];
+    # });
     ephemeralpg = pkgs.ephemeralpg.overrideAttrs(oldAttrs: {
       installPhase = ''
         mkdir -p $out
