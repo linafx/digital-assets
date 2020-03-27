6e068d1792fcc73157e26cb641c4e165c5135446
Marcin Ziolek <marcin.ziolek@digitalasset.com>
2020-03-27 17:26:07 +0100

    temp workaround

--- ledger/sandbox/src/main/scala/com/digitalasset/platform/ApiOffset.scala
+++ ledger/sandbox/src/main/scala/com/digitalasset/platform/ApiOffset.scala
@@ -6,22 +6,39 @@ package com.digitalasset.platform
 import com.daml.ledger.participant.state.v1.Offset
 import com.digitalasset.daml.lf.data.Ref
 
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
 object ApiOffset {
 
+  private val Bytes = 16
+  private val MaxValue = BigInt(Array.fill(Bytes)(0xff.toByte).updated(0, 0x7f.toByte))
+  private val Base10Digits = MaxValue.toString.length
+  private val InvalidOffsetErrorMessage = s"Offset is negative or does not fit in $Bytes bytes"
+
   def fromString(s: String): Try[Offset] =
-    Ref.HexString
-      .fromString(s)
-      .fold(
-        err => Failure(new IllegalArgumentException(err)),
-        b => Success(Offset.fromHexString(b))
+    Try {
+      val asBigInt = BigInt(s)
+      val unpadded = asBigInt.toByteArray
+      require(asBigInt >= 0 && unpadded.length <= Bytes, InvalidOffsetErrorMessage)
+      val padded = Array.ofDim[Byte](Bytes)
+      Array.copy(
+        src = unpadded,
+        srcPos = 0,
+        dest = padded,
+        destPos = Bytes - unpadded.length,
+        length = unpadded.length,
       )
+      Offset.fromByteArray(padded)
+    }
 
   def assertFromString(s: String): Offset = fromString(s).get
 
-  def toApiString(offset: Offset): Ref.LedgerString =
-    offset.toHexString
+  def toApiString(offset: Offset): Ref.LedgerString = {
+    val asBigInt = BigInt(offset.toByteArray)
+    require(asBigInt >= 0 && asBigInt <= MaxValue, InvalidOffsetErrorMessage)
+    val unpadded = asBigInt.toString
+    Ref.LedgerString.assertFromString(("0" * (Base10Digits - unpadded.length)) + unpadded)
+  }
 
   implicit class ApiOffsetConverter(val offset: Offset) {
     def toApiString: Ref.LedgerString = ApiOffset.toApiString(offset)
