c6d9a0e2003e2690b4f9c3c8ae69c44877f5da37
Gert-Jan Bottu <gertjan.bottu@kuleuven.be>
2020-05-29 09:09:35 +0200

    Various improvements: Support tuples of cid's, Bind self correctly, etc.

--- compiler/daml-lf-verify/src/DA/Daml/LF/Verify/Subst.hs
+++ compiler/daml-lf-verify/src/DA/Daml/LF/Verify/Subst.hs
@@ -5,6 +5,7 @@
 module DA.Daml.LF.Verify.Subst
   ( ExprSubst
   , emptyExprSubst
+  , concatExprSubst
   , singleExprSubst
   , singleTypeSubst
   , createExprSubst
@@ -24,6 +25,10 @@ type ExprSubst = Map.Map ExprVarName Expr
 -- | Constructs an empty substition.
 emptyExprSubst :: ExprSubst
 emptyExprSubst = Map.empty
+
+-- | Combine two substitions.
+concatExprSubst :: ExprSubst -> ExprSubst -> ExprSubst
+concatExprSubst = Map.union
 
 -- | Create an expression substitution from a single variable and expression.
 singleExprSubst :: ExprVarName
