42457e25481c1a25897b242155221888066f0974
Andreas Herrmann <andreas.herrmann@tweag.io>
2020-09-28 13:22:51 +0200

    Test that the OAuth2 test server retains query parameters
    changelog_begin
    changelog_end

--- triggers/service/auth/src/test/scala/com/daml/oauth/server/Test.scala
+++ triggers/service/auth/src/test/scala/com/daml/oauth/server/Test.scala
@@ -5,7 +5,7 @@ package com.daml.oauth.server
 
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.Uri.Path
+import akka.http.scaladsl.model.Uri.{Path, Query}
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.Location
 import akka.http.scaladsl.unmarshalling.Unmarshal
@@ -13,24 +13,27 @@ import com.daml.jwt.JwtDecoder
 import com.daml.jwt.domain.Jwt
 import com.daml.ledger.api.auth.{AuthServiceJWTCodec, AuthServiceJWTPayload}
 import com.daml.ledger.api.testing.utils.SuiteResourceManagementAroundAll
-import org.scalatest.AsyncWordSpec
+import org.scalatest.{AsyncWordSpec, Matchers}
 import spray.json._
 
 import scala.concurrent.Future
 
-class Test extends AsyncWordSpec with TestFixture with SuiteResourceManagementAroundAll {
+class Test extends AsyncWordSpec with TestFixture with SuiteResourceManagementAroundAll with Matchers {
   import Client.JsonProtocol._
-  private def requestToken(parties: Seq[String]): Future[AuthServiceJWTPayload] = {
+  private def requestAccess(parties: Seq[String], query: Query = Query()) = {
     lazy val clientBinding = suiteResource.value._2.localAddress
     lazy val clientUri = Uri().withAuthority(clientBinding.getHostString, clientBinding.getPort)
     val req = HttpRequest(
-      uri = clientUri.withPath(Path./("access")).withScheme("http"),
+      uri = clientUri.withPath(Path./("access")).withScheme("http").withQuery(query),
       method = HttpMethods.POST,
       entity =
         HttpEntity(MediaTypes.`application/json`, Client.AccessParams(parties).toJson.compactPrint)
     )
+    Http().singleRequest(req)
+  }
+  private def requestToken(parties: Seq[String]): Future[AuthServiceJWTPayload] = {
     for {
-      resp <- Http().singleRequest(req)
+      resp <- requestAccess(parties)
       // Redirect to /authorize on authorization server (No automatic redirect handling in akka-http)
       resp <- {
         assert(resp.status == StatusCodes.Found)
@@ -72,6 +75,24 @@ class Test extends AsyncWordSpec with TestFixture with SuiteResourceManagementAr
         token <- requestToken(Seq("Alice", "Bob"))
       } yield {
         assert(token.actAs == Seq("Alice", "Bob"))
+      }
+    }
+    "preserve a query string on the redirect URI" in {
+      // The OAuth2 specification mandates that query parameters in the
+      // redirect URI must be retained. This tests that the OAuth2 test server
+      // follows that specification.
+      val query = Query(("test", "parameter"))
+      for {
+        resp <- requestAccess(Seq(), query)
+        // Redirect to /authorize on authorization server
+        resp <- {
+          assert(resp.status == StatusCodes.Found)
+          val req = HttpRequest(uri = resp.header[Location].get.uri)
+          Http().singleRequest(req)
+        }
+      } yield {
+        assert(resp.status == StatusCodes.Found)
+        resp.header[Location].get.uri.query().toList should contain allElementsOf (query.toList)
       }
     }
   }
