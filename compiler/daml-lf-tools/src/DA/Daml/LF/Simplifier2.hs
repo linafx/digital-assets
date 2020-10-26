21ea0156ef69d6a8ff0d04e743ad5feaed6428fa
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-10-26 21:14:05 +0100

    Name intermediate variables better
    CHANGELOG_BEGIN
    CHANGELOG_END

--- compiler/daml-lf-tools/src/DA/Daml/LF/Simplifier2.hs
+++ compiler/daml-lf-tools/src/DA/Daml/LF/Simplifier2.hs
@@ -3,17 +3,22 @@ module DA.Daml.LF.Simplifier2 (
     simplifyModule
 ) where
 
+import Control.Monad.State.Strict
 import DA.Daml.LF.Ast
 import Data.Functor.Foldable
 import Data.Map.Strict (Map)
-import Data.Maybe (fromJust)
 import Data.Set (Set)
-import Data.Traversable (mapAccumL)
-import qualified Data.List as List
+import Data.Tuple (swap)
 import qualified Data.Map.Strict as Map
 import qualified Data.NameMap as NM
 import qualified Data.Set as Set
 import qualified Data.Text.Extended as T
+
+data AnfState = AnfState
+    { freshVarCounter :: Int
+    }
+
+type AnfM = State AnfState
 
 simplifyModule :: World -> Version -> Module -> Module
 simplifyModule world version m =
@@ -23,33 +28,40 @@ simplifyDefValue :: World -> Version -> DefValue -> DefValue
 simplifyDefValue world version d = d{dvalBody = simplifyExpr world version (dvalBody d)}
 
 simplifyExpr :: World -> Version -> Expr -> Expr
-simplifyExpr _ _ = anf Set.empty Map.empty
+simplifyExpr _ _ e = runAnfM (anf Set.empty Map.empty e)
 
-anf :: Set ExprVarName -> Map ExprVarName ExprVarName -> Expr -> Expr
-anf fvs ren e0 =
-    let (bs, e1) = anf' fvs ren e0 in mkELets bs e1
+runAnfM :: AnfM a -> a
+runAnfM act = evalState act AnfState
+    { freshVarCounter = 0
+    }
 
-anf' :: Set ExprVarName -> Map ExprVarName ExprVarName -> Expr -> ([Binding], Expr)
+anf :: Set ExprVarName -> Map ExprVarName ExprVarName -> Expr -> AnfM Expr
+anf fvs ren e0 = do
+    (bs, e1) <- anf' fvs ren e0
+    pure $ mkELets bs e1
+
+anf' :: Set ExprVarName -> Map ExprVarName ExprVarName -> Expr -> AnfM ([Binding], Expr)
 anf' fvs ren e = case e of
-    EVar x -> ([], EVar (Map.findWithDefault x x ren))
-    ETmLam (x, t) e' -> ([], ETmLam (x, t) (anf (Set.insert x fvs) ren e'))
-    ETyLam (t, k) e' -> ([], ETyLam (t, k) (anf fvs ren e'))
-    ELet (Binding (x, t) e1) e2 ->
-        let (bs1, e1') = anf' fvs ren e1 in
-        let (x', ren')
-              | x `Set.member` fvs = let x' = freshName fvs in (x', Map.insert x x' ren)
-              | otherwise = (x, ren)
-        in
-        let (bs2, e2') = anf' (Set.insert x' fvs) ren' e2 in
-        (bs1 ++ [Binding (x', t) e1'] ++ bs2, e2')
-    ECase e0 as0 ->
-        let (bs1, e1) = anf' fvs ren e0 in
-        let (bs2, e2)
-              | isAtomic e1 = (bs1, e1)
-              | otherwise =
-                let x = freshName fvs in
-                (bs1 ++ [Binding (x, Nothing) e1], EVar x)
-        in
+    EVar x -> pure ([], EVar (Map.findWithDefault x x ren))
+    ETmLam (x, t) e' -> ([],) . ETmLam (x, t) <$> anf (Set.insert x fvs) ren e'
+    ETyLam (t, k) e' -> ([],) . ETyLam (t, k) <$> anf fvs ren e'
+    ELet (Binding (x, t) e1) e2 -> do
+        (bs1, e1') <- anf' fvs ren e1
+        (x', ren') <- if x `Set.member` fvs
+            then do
+                x' <- freshName
+                pure (x', Map.insert x x' ren)
+            else
+                pure (x, ren)
+        (bs2, e2') <- anf' (Set.insert x' fvs) ren' e2
+        pure (bs1 ++ [Binding (x', t) e1'] ++ bs2, e2')
+    ECase e0 as0 -> do
+        (bs1, e1) <- anf' fvs ren e0
+        (bs2, e2) <- if isAtomic e1
+            then pure (bs1, e1)
+            else do
+                x <- freshName
+                pure (bs1 ++ [Binding (x, Nothing) e1], EVar x)
         let fvPat p = case p of
                 CPVariant _ _ x -> Set.singleton x
                 CPEnum _ _ -> Set.empty
@@ -60,15 +72,14 @@ anf' fvs ren e = case e of
                 CPNone -> Set.empty
                 CPSome x -> Set.singleton x
                 CPDefault -> Set.empty
-        in
-        let anfAlt (CaseAlternative p e) = CaseAlternative p (anf (fvs `Set.union` fvPat p) ren e) in
-        let as1 = map anfAlt as0 in
-        (bs2, ECase e2 as1)
-    ETyApp e0 t ->
-        let (bs, e1) = anf' fvs ren e0 in
-        (bs, ETyApp e1 t)
-    EUpdate{} -> ([], e)
-    EScenario{} -> ([], e)
+        let anfAlt (CaseAlternative p e) = CaseAlternative p <$> anf (fvs `Set.union` fvPat p) ren e
+        as1 <- traverse anfAlt as0
+        pure (bs2, ECase e2 as1)
+    ETyApp e0 t -> do
+        (bs, e1) <- anf' fvs ren e0
+        pure (bs, ETyApp e1 t)
+    EUpdate{} -> pure ([], e)
+    EScenario{} -> pure ([], e)
     ELocation _ e' -> anf' fvs ren e'
     EVal{} -> defaultAnf
     EBuiltin{} -> defaultAnf
@@ -89,20 +100,21 @@ anf' fvs ren e = case e of
     EFromAny{} -> defaultAnf
     ETypeRep{} -> defaultAnf
   where
-    defaultAnf =
-        let step fvs e0 =
-                let (bs, e1) = anf' fvs ren e0 in
+    defaultAnf = do
+        let step fvs e0 = do
+                (bs, e1) <- anf' fvs ren e0
                 if isAtomic e1 then
-                    (fvs, (bs, e1))
-                else
-                    let x = freshName fvs in
-                    (Set.insert x fvs, (bs ++ [Binding (x, Nothing) e1], EVar x))
-        in
-        let (_, bses) = mapAccumL step fvs (project e) in
-        (concatMap fst bses, embed (fmap snd bses))
+                    pure (fvs, (bs, e1))
+                else do
+                    x <- freshName
+                    pure (Set.insert x fvs, (bs ++ [Binding (x, Nothing) e1], EVar x))
+        (_, bses) <- mapAccumLM step fvs (project e)
+        pure (concatMap fst bses, embed (fmap snd bses))
 
-freshName :: Set ExprVarName -> ExprVarName
-freshName fvs = fromJust (List.find (`Set.notMember` fvs) [ExprVarName ("$v" <> T.show n) | n <- [1 :: Int ..]])
+freshName :: AnfM ExprVarName
+freshName = do
+    n <- state (\st -> let k = freshVarCounter st + 1 in (k, st{freshVarCounter = k}))
+    pure (ExprVarName ("$v" <> T.show n))
 
 isAtomic :: Expr -> Bool
 isAtomic e = case e of
@@ -112,3 +124,10 @@ isAtomic e = case e of
     ENone{} -> True
     ETyApp e' _ -> isAtomic e'
     _ -> False
+
+-- | Monadic version of mapAccumL
+mapAccumLM :: forall t m acc x y. (Traversable t, Monad m) =>
+    (acc -> x -> m (acc, y)) -> acc -> t x -> m (acc, t y)
+mapAccumLM f z0 xs =
+    let g x = StateT $ \z -> swap <$> f z x in
+    swap <$> runStateT (traverse g xs) z0
