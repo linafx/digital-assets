720531d9b9978ef66f47007830f48b019f4da03b
Marton Nagy <marton.nagy@digitalasset.com>
2021-03-01 08:56:51 +0100

    Makes compression configurable

--- ledger/participant-integration-api/src/main/scala/platform/indexer/IndexerConfig.scala
+++ ledger/participant-integration-api/src/main/scala/platform/indexer/IndexerConfig.scala
@@ -26,6 +26,7 @@ case class IndexerConfig(
     tailingRateLimitPerSecond: Int = 100,
     batchWithinMillis: Long = 10,
     runStageUntil: Int = 6,
+    enableCompression: Boolean = true,
 )
 
 object IndexerConfig {
