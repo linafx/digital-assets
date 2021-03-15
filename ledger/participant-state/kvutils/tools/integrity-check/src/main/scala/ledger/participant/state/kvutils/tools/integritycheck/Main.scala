shalinigaur-da/daml

Indexer flow-level testing enhancements
-pull out indexer testing from integrity checker code
-direct inject functionality of conversion
-add features:
  -streaming
  -split initialisation from perf-test
  -throughput metrics

ledger/participant-state/kvutils/tools/integrity-check/src/main/scala/ledger/participant/state/kvutils/tools/integritycheck/Main.scala
@@ -9,7 +9,7 @@ import scala.concurrent.ExecutionContext
 
 object Main {
   def main(args: Array[String]): Unit =
-    IntegrityChecker.run(args, commitStrategySupportFactory _)
+    IntegrityChecker.run(args, commitStrategySupportFactory _, None)
 
   private def commitStrategySupportFactory(metrics: Metrics, executionContext: ExecutionContext) =
     new LogAppendingCommitStrategySupport(metrics)(executionContext)

linafx/digital-assets

ledger/participant-state/kvutils/tools/integrity-check/src/main/scala/ledger/participant/state/kvutils/tools/integritycheck/Main.scala
@@ -9,7 +9,7 @@ import scala.concurrent.ExecutionContext
 
 object Main {
   def main(args: Array[String]): Unit =
-    IntegrityChecker.runAndExit(args, preExecutionCommitStrategySupportFactory _)
+    IntegrityChecker.runAndExit(args, preExecutionCommitStrategySupportFactory _, None)
 
   def batchingCommitStrategySupportFactory(
       metrics: Metrics,